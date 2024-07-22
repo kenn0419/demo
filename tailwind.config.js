2030082d0b67fe2640b20d84d07f9acadf64b481
Quoc Bao <tranbao7008@gmail.com>
2024-07-22 10:49:15 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      width: {
+        main: "1200px",
+      },
+    },
+  },
+  plugins: [],
+};
